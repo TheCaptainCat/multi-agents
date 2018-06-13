02a95a9765f18eefe5a5573e2430b6ad66ab192a
Pierre Chat <pierrechat89@hotmail.fr>
2018-06-13 10:19:20 +0200

    Display + moving agents

--- src/main/java/fr/bachachat/multiagents/logic/Agent.java
+++ src/main/java/fr/bachachat/multiagents/logic/Agent.java
@@ -1,14 +1,21 @@
 package fr.bachachat.multiagents.logic;
 
 public class Agent implements Runnable {
+    private static int IDS = 0;
+    private static long PAUSE_TIME = 1000;
+
+    private int id;
     private Vector position;
     private Vector destination;
     private Grid grid;
+    private boolean running;
 
     public Agent(Vector position, Vector destination, Grid grid) {
+        this.id = Agent.IDS++;
         this.position = position;
         this.destination = destination;
         this.grid = grid;
+        this.running = false;
     }
 
     public Vector getPosition() {
@@ -31,8 +38,27 @@ public class Agent implements Runnable {
         this.position = new Vector(x, y);
     }
 
-    @Override
-    public void run() {
+    public boolean isRunning() {
+        return running;
+    }
 
+    public void stop() {
+        this.running = false;
+    }
+
+    @Override
+    public synchronized void run() {
+        this.running = true;
+        try {
+            while (this.running) {
+                Thread.sleep(Agent.PAUSE_TIME);
+                if (!this.position.equals(this.destination)) {
+                    Vector vector = Pathfinding.nextPosition(this.grid, this);
+                    this.grid.moveAgent(this, vector);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
